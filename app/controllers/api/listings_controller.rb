b35727cc81c40662a61a4c8dc971cccf808f5631
Owen Luu <owenluu111@gmail.com>
2021-03-04 20:50:48 -0600

    made some updates to my index controller to be able to search

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,18 +1,52 @@
 class Api::ListingsController < ApplicationController
   def index
     @listing = Listing.all
+
+    if params[:title_search_terms]
+      @listing = @listing.where("title ILIKE ?", "%#{params[:title_search_terms]}%")
+    end
+
+    @listing = @listing.order(:id => :desc)
+
     render "index.json.jb"
   end
 
   def create
+    @listing = Listing.new(
+      title: params["title"],
+      description: params["description"],
+      user_id: params["user_id"],
+      price: params["price"],
+      quantity: params["quantity"],
+      location_id: params["location_id"],
+      shoe_brand_id: params["shoe_brand_id"],
+    )
+    @listing.save
+    render "show.json.jb"
   end
 
   def show
+    @listing = Listing.find_by(id: params["id"])
+    render "show.json.jb"
   end
 
   def update
+    @listing = Listing.find_by(id: params[:id])
+    @listing.title = params["title"] || @listing.title
+    @listing.description = params["description"] || @listing.description
+    @listing.user_id = params["user_id"] || @listing.user_id
+    @listing.price = params["price"] || @listing.price
+    @listing.quantity = params["quantity"] || @listing.quantity
+    @listing.availability = params["availability"] || @listing.availability
+    @listing.location_id = params["location_id"] || @listing.location_id
+    @listing.shoe_brand_id = params["shoe_brand_id"] || @listing.shoe_brand_id
+    @listing.save
+    render "show.json.jb"
   end
 
   def destroy
+    listing = Listing.find_by(id: params["id"])
+    listing.destroy
+    render json: { message: "Listing Sucessfully Deleted!" }
   end
 end
