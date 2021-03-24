a2fb9ca82fdd71a9ce2db282259c20891b0bb27b
Owen Luu <owenluu111@gmail.com>
2021-03-23 21:42:46 -0500

    prototype is done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  #finally done :)
   namespace :api do
     post "/users" => "users#create"
     get "/users/:id" => "users#show"
